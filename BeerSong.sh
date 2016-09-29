b3a9ed2adb405fdf6c77f02ad7fbd48ad6b13174
Michael Opiola <maopiola@oakland.edu>
2016-09-29 15:48:51 -0400

    BeerSong.sh had some code tidied and reorganized.

--- BeerSong.sh
+++ BeerSong.sh
@@ -12,35 +12,35 @@
 function Main {
     Ninety_Nine_Bottles_of_Beer
         }
-		#function that uses a for loop and if conditions to print out the song
-		    function Ninety_Nine_Bottles_of_Beer {
 
-		        #for loop from 99 to 1 for the length of the song
-			        for i in {99..1}; do
+	#function that uses a for loop and if conditions to print out the song
+	function Ninety_Nine_Bottles_of_Beer {
+	
+	#for loop from 99 to 1 for the length of the song
+        for i in {99..1}; do
 
-					#normal print out of x and x-1 as long as the loop is greater than 2
-							if [ $i -gt 2 ]; then 
-										bottles="$i bottles"
-													bottles2="$(( $i-1 )) bottles"
+		#normal print out of x and x-1 as long as the loop is greater than 2
+		if [ $i -gt 2 ]; then 
+	
+			bottles="$i bottles"
+			bottles2="$(( $i-1 )) bottles"
 
-															#specific conditions for equal to 2 and 1 for specific outputs
-																	elif [ $i -eq 2 ]; then
-																				bottles="2 bottles"
-																							bottles2="1 bottle"
-																									elif [ $i -eq 1 ]; then
-																												bottles="1 bottle"
-																															bottles2="no more bottles"
-																																	fi
+		#specific conditions for equal to 2 and 1 for specific outputs
+		elif [ $i -eq 2 ]; then
+			bottles="2 bottles"
+			bottles2="1 bottle"
+		elif [ $i -eq 1 ]; then
+			bottles="1 bottle"
+			bottles2="no more bottles"
+		fi
 																																			
 
-																																					#output the song after getting correct values of bottles
-																																							echo "$bottles of beer on the wall, $bottles of beer"
-																																									echo "take one down, pass it around, $bottles2 of beer on the wall."
+																									#output the song after getting correct values of bottle	
+																									echo "$bottles of beer on the wall, $bottles of beer"
+	echo "take one down, pass it around, $bottles2 of beer on the wall."
+	done
 
-																																										done
-
-																																											}
-
-																																											#call main to initiate the script
-																																											Main
+																									}
+																									#call main to initiate the script
+																									Main
 
